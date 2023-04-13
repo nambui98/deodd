e98005d5ee085214bad3cbc0205077b0e402c16d
Hà Ngọc Minh <hangoc.minh1306@gmail.com>
2023-04-13 14:25:33 +0700

    refactor a bit for smaller size

--- pages/statistic.tsx
+++ pages/statistic.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Container, Typography, Grid, SvgIcon } from "@mui/material";
+import { Box, Container, Typography, Grid } from "@mui/material";
 import Image from "next/image";
 import {
   LogoImage,
@@ -15,7 +15,67 @@ import { FlipPerUserTable } from "components/common/FlipPerUserTable";
 
 type Props = {};
 
-function Statistic({}: Props) {
+function TitleTextAbsolute({ text }: { text: string }) {
+  return (
+    <Typography
+      top={"1rem"}
+      left={"1rem"}
+      position={"absolute"}
+      textTransform={"uppercase"}
+      variant="body2"
+    >
+      {text}
+    </Typography>
+  );
+}
+
+function CardType01({
+  children,
+  bgimg,
+}: {
+  children: React.ReactNode;
+  bgimg: {
+    backgroundImage: string;
+    backgroundSize?: string;
+    backgroundRepeat?: string;
+    backgroundPosition?: string;
+  };
+}) {
+  return (
+    <Box
+      p={2}
+      bgcolor={"secondary.300"}
+      borderRadius={1.5}
+      height={"15.5rem"}
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      position={"relative"}
+      sx={bgimg}
+    >
+      {children}
+    </Box>
+  );
+}
+
+function CardType03({ children }: { children: React.ReactNode }) {
+  return (
+    <Box
+      p={2}
+      bgcolor={"secondary.300"}
+      borderRadius={1.5}
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      position={"relative"}
+      height={"15.375rem"}
+    >
+      {children}
+    </Box>
+  );
+}
+
+export default function Statistic({}: Props) {
   return (
     <Container sx={{ mt: 5, mb: 10 }}>
       <Typography variant="h2" mb={3}>
@@ -23,29 +83,13 @@ function Statistic({}: Props) {
       </Typography>
       <Grid container spacing={2}>
         <Grid item md={6} xs={12}>
-          <Box
-            p={2}
-            bgcolor={"secondary.300"}
-            borderRadius={1.5}
-            height={"15.5rem"}
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-            position={"relative"}
-            sx={{
+          <CardType01
+            bgimg={{
               backgroundImage: `url(${bgWinStreakImage})`,
               backgroundSize: "cover",
             }}
           >
-            <Typography
-              top={"1rem"}
-              left={"1rem"}
-              position={"absolute"}
-              textTransform={"uppercase"}
-              variant="body2"
-            >
-              highest win streak
-            </Typography>
+            <TitleTextAbsolute text="highest win streak" />
             <Box
               display={"flex"}
               gap={1}
@@ -56,35 +100,19 @@ function Statistic({}: Props) {
               <Typography variant="h1">08</Typography>
               <Typography>Linh (38957***bF1)</Typography>
             </Box>
-          </Box>
+          </CardType01>
         </Grid>
         <Grid item md={6} xs={12}>
-          <Box
-            p={2}
-            bgcolor={"secondary.300"}
-            borderRadius={1.5}
-            height={"15.5rem"}
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-            position={"relative"}
-            sx={{
+          <CardType01
+            bgimg={{
               backgroundImage: `url(${bgLossStreakImage})`,
               backgroundRepeat: "no-repeat",
               backgroundPosition: "bottom",
             }}
           >
-            <Typography
-              top={"1rem"}
-              left={"1rem"}
-              position={"absolute"}
-              textTransform={"uppercase"}
-              variant="body2"
-            >
-              highest loss streak
-            </Typography>
+            <TitleTextAbsolute text="highest loss streak" />
             <Typography variant="h1">05</Typography>
-          </Box>
+          </CardType01>
         </Grid>
         <Grid item md={4} xs={12}>
           <Box
@@ -253,15 +281,7 @@ function Statistic({}: Props) {
             justifyContent={"center"}
             alignItems={"center"}
           >
-            <Typography
-              textTransform={"uppercase"}
-              position={"absolute"}
-              top={"1rem"}
-              left={"1rem"}
-              variant="body2"
-            >
-              flip total
-            </Typography>
+            <TitleTextAbsolute text="flip total" />
             <Box
               display={"flex"}
               justifyContent={"center"}
@@ -285,27 +305,16 @@ function Statistic({}: Props) {
           </Box>
         </Grid>
 
-        <Grid item md={4} xs={12}>
-          <Box
-            p={2}
-            bgcolor={"secondary.300"}
-            borderRadius={1.5}
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-            position={"relative"}
-            height={"15.375rem"}
-            mb={2}
-          >
-            <Typography
-              position={"absolute"}
-              top={"1rem"}
-              left={"1rem"}
-              textTransform={"uppercase"}
-              variant="body2"
-            >
-              fee total
-            </Typography>
+        <Grid
+          item
+          md={4}
+          xs={12}
+          display={"flex"}
+          flexDirection={"column"}
+          gap={2}
+        >
+          <CardType03>
+            <TitleTextAbsolute text="fee total" />
             <Box>
               <Typography mt={4} variant="h1">
                 52.645{" "}
@@ -330,26 +339,10 @@ function Statistic({}: Props) {
                 12%
               </Typography>
             </Box>
-          </Box>
-          <Box
-            p={2}
-            bgcolor={"secondary.300"}
-            borderRadius={1.5}
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-            position={"relative"}
-            height={"15.375rem"}
-          >
-            <Typography
-              position={"absolute"}
-              top={"1rem"}
-              left={"1rem"}
-              textTransform={"uppercase"}
-              variant="body2"
-            >
-              bnb total
-            </Typography>
+          </CardType03>
+
+          <CardType03>
+            <TitleTextAbsolute text="bnb total" />
             <Box>
               <Typography mt={4} variant="h1">
                 164,242{" "}
@@ -374,7 +367,7 @@ function Statistic({}: Props) {
                 24%
               </Typography>
             </Box>
-          </Box>
+          </CardType03>
         </Grid>
 
         <Grid item md={8} xs={12}>
@@ -398,25 +391,8 @@ function Statistic({}: Props) {
           </Box>
         </Grid>
         <Grid item md={4} xs={12}>
-          <Box
-            bgcolor={"secondary.300"}
-            borderRadius={1.5}
-            p={2}
-            height={"15.375rem"}
-            position={"relative"}
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-          >
-            <Typography
-              textTransform={"uppercase"}
-              position={"absolute"}
-              top={"1rem"}
-              left={"1rem"}
-              variant="body2"
-            >
-              win percentage
-            </Typography>
+          <CardType03>
+            <TitleTextAbsolute text="win percentage" />
             <Box display={"flex"} alignItems={"center"} gap={1}>
               <CupIcon fill={Colors.primaryDark} width={"2.5rem"} />
               <Typography variant="h1">
@@ -426,28 +402,11 @@ function Statistic({}: Props) {
                 </Typography>
               </Typography>
             </Box>
-          </Box>
+          </CardType03>
         </Grid>
         <Grid item md={4} xs={12}>
-          <Box
-            bgcolor={"secondary.300"}
-            borderRadius={1.5}
-            p={2}
-            height={"15.375rem"}
-            position={"relative"}
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-          >
-            <Typography
-              textTransform={"uppercase"}
-              position={"absolute"}
-              top={"1rem"}
-              left={"1rem"}
-              variant="body2"
-            >
-              mobile flips
-            </Typography>
+          <CardType03>
+            <TitleTextAbsolute text="mobile flips" />
             <Box display={"flex"} alignItems={"center"} gap={1}>
               <MobileIcon fill={Colors.primary} width={"2.5rem"} />
               <Typography variant="h1">
@@ -457,7 +416,7 @@ function Statistic({}: Props) {
                 </Typography>
               </Typography>
             </Box>
-          </Box>
+          </CardType03>
         </Grid>
         <Grid item md={4} xs={12}>
           <Box
@@ -474,5 +433,3 @@ function Statistic({}: Props) {
     </Container>
   );
 }
-
-export default Statistic;
