5dad8f021c2b98c286fc421cd5bfe82ed70c84f9
Nam Bui <bvnam98@gmail.com>
2023-04-22 16:05:00 +0700

    add comming soon for lottery, profile

---
+++ pages/lottery.tsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+type Props = {}
+
+function Lottery({ }: Props) {
+    return (
+        <Typography variant='h2' mx="auto" mt={4} textAlign={'center'}>
+            Comming soon
+        </Typography>
+    )
+}
+
+export default Lottery
