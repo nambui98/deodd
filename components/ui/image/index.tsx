080bfe1843ed45f0f004fc6ab7f3ec3730590fb3
Hà Ngọc Minh <hangoc.minh1306@gmail.com>
2023-04-25 09:45:35 +0700

    style: fix volume icon turn larger
    This fixes the mismatch icon size of volume off and volume on

--- components/ui/image/index.tsx
+++ components/ui/image/index.tsx
@@ -2,15 +2,16 @@ import Image from 'next/image'
 import { Box, BoxProps } from '@mui/material'
 import React from 'react'
 
-type Props = BoxProps & {
-    src: string,
-    alt: string
 
+type Props = BoxProps & {
+    alt: string;
+    src: string;
 }
 
-function MyImage({ src, alt, ...props }: Props) {
+
+function MyImage({ src, alt, width, height, ...props }: Props) {
     return (
-        <Box position={'relative'} {...props}>
+        <Box position={'relative'} width={width} height={height} {...props}>
             <Image alt={alt} fill src={src} style={{ objectFit: 'contain' }} />
         </Box>
     )
