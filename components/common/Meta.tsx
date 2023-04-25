b8683c85f3a89ce0890b0eedf5eeacfddf18cb2e
Nam bv <nambv@versehub.io>
2023-04-25 09:27:16 +0700

    add meta

---
+++ components/common/Meta.tsx
@@ -0,0 +1,84 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { NextSeo } from 'next-seo';
+
+import { AppConfig } from '@/utils/AppConfig';
+
+type IMetaProps = {
+  title: string;
+  description: string;
+  canonical?: string;
+};
+
+const Meta = (props: IMetaProps) => {
+  const router = useRouter();
+
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" key="charset" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1"
+          key="viewport"
+        />
+        <meta property="og:title" content="Live the Metaverse life!"></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:url" content="https://www.penpalnft.com/"></meta>
+        <meta
+          property="og:description"
+          content="Penpal is an NFT collection that helps promote a healthy and open mindset about your own and others' body image."
+        ></meta>
+        <meta
+          property="og:image"
+          content="/assets/images/thumbnail.jpeg"
+        ></meta>
+        <link
+          rel="apple-touch-icon"
+          href={`${router.basePath}/apple-touch-icon.png`}
+          key="apple"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={`${router.basePath}/favicon-32x32.png`}
+          key="icon32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`${router.basePath}/favicon-16x16.png`}
+          key="icon16"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="24x24"
+          href={`${router.basePath}/favicon-24x24.png`}
+          key="icon16"
+        />
+        <link
+          rel="icon"
+          href={`${router.basePath}/favicon.ico`}
+          key="favicon"
+        />
+      </Head>
+      <NextSeo
+        title={props.title}
+        description={props.description}
+        canonical={props.canonical}
+        openGraph={{
+          title: props.title,
+          description: props.description,
+          url: props.canonical,
+          locale: AppConfig.locale,
+          site_name: AppConfig.site_name,
+        }}
+      />
+    </>
+  );
+};
+
+export { Meta };
