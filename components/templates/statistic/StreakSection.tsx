3047a38b4cc892a63257fff9a3b8fa976c27660c
Hà Ngọc Minh <hangoc.minh1306@gmail.com>
2023-05-11 17:51:27 +0700

    refactor: replace mui grid with css grid
    Using CSS Grid for dashboard layout instead of using MUI Grid

--- components/templates/statistic/StreakSection.tsx
+++ components/templates/statistic/StreakSection.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography, Box } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 import { bgWinStreakImage, bgLossStreakImage } from "utils/Images";
 import { DashboardCard } from "./DashboardCard";
 import { TitleTextAbsolute } from "./TitleTextAbsolute";
@@ -15,66 +15,64 @@ type StreakPropsType = {
 export function StreakSection({ error, streak }: StreakPropsType) {
   return (
     <>
-      <Grid item md={6} xs={12}>
-        <DashboardCard
+      <DashboardCard
+        gridColumn={{ md: "auto / span 3", xs: "auto / span 6" }}
+        justifyContent={"center"}
+        position={"relative"}
+        height="13.5rem"
+        sx={{
+          backgroundImage: `url(${bgWinStreakImage})`,
+          backgroundSize: "cover",
+        }}
+      >
+        <TitleTextAbsolute text="highest win streak" />
+        <Box
+          display={"flex"}
+          gap={1}
+          flexDirection={"column"}
           justifyContent={"center"}
-          position={"relative"}
-          height="13.5rem"
-          sx={{
-            backgroundImage: `url(${bgWinStreakImage})`,
-            backgroundSize: "cover",
-          }}
+          alignItems={"center"}
         >
-          <TitleTextAbsolute text="highest win streak" />
-          <Box
-            display={"flex"}
-            gap={1}
-            flexDirection={"column"}
-            justifyContent={"center"}
-            alignItems={"center"}
-          >
-            {error.haveFlipped ? (
-              <>
-                <Typography variant="h1" fontSize={"3rem"} lineHeight={"3.795rem"}>
-                  {streak.winStreak < 10 && streak.winStreak >= 1
-                    ? `0${streak.winStreak}`
-                    : streak.winStreak}
-                </Typography>
-                <Typography fontSize={"1rem"} textAlign={"center"}>
-                  {streak.username ? streak.username : "Unknown"}
-                  {" "}
-                  {streak.winWallet ? `(${Convert.convertWalletAddress(streak.winWallet, 5, 4)})` : ""}
-                </Typography>
-              </>
-            ) : (
-              <Typography variant="h3">{error.errorMessage}</Typography>
-            )}
-          </Box>
-        </DashboardCard>
-      </Grid>
-      <Grid item md={6} xs={12}>
-        <DashboardCard
-          justifyContent={"center"}
-          position={"relative"}
-          height="13.5rem"
-          sx={{
-            backgroundImage: `url(${bgLossStreakImage})`,
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "bottom",
-          }}
-        >
-          <TitleTextAbsolute text="highest loss streak" />
           {error.haveFlipped ? (
-            <Typography variant="h1" fontSize={"3rem"} lineHeight={"3.795rem"}>
-              {streak.lossStreak < 10 && streak.lossStreak >= 1
-                ? `0${streak.lossStreak}`
-                : streak.lossStreak}
-            </Typography>
+            <>
+              <Typography variant="h1" fontSize={"3rem"} lineHeight={"3.795rem"}>
+                {streak.winStreak < 10 && streak.winStreak >= 1
+                  ? `0${streak.winStreak}`
+                  : streak.winStreak}
+              </Typography>
+              <Typography fontSize={"1rem"} textAlign={"center"}>
+                {streak.username ? streak.username : "Unknown"}
+                {" "}
+                {streak.winWallet ? `(${Convert.convertWalletAddress(streak.winWallet, 5, 4)})` : ""}
+              </Typography>
+            </>
           ) : (
             <Typography variant="h3">{error.errorMessage}</Typography>
           )}
-        </DashboardCard>
-      </Grid>
+        </Box>
+      </DashboardCard>
+      <DashboardCard
+        gridColumn={{ md: "auto / span 3", xs: "auto / span 6" }}
+        justifyContent={"center"}
+        position={"relative"}
+        height="13.5rem"
+        sx={{
+          backgroundImage: `url(${bgLossStreakImage})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "bottom",
+        }}
+      >
+        <TitleTextAbsolute text="highest loss streak" />
+        {error.haveFlipped ? (
+          <Typography variant="h1" fontSize={"3rem"} lineHeight={"3.795rem"}>
+            {streak.lossStreak < 10 && streak.lossStreak >= 1
+              ? `0${streak.lossStreak}`
+              : streak.lossStreak}
+          </Typography>
+        ) : (
+          <Typography variant="h3">{error.errorMessage}</Typography>
+        )}
+      </DashboardCard>
     </>
   );
 }
