c3e0559fbb90e7e05b6d61740a714317863f452a
Hà Ngọc Minh <hangoc.minh1306@gmail.com>
2023-05-17 11:46:47 +0700

    style: change FAQ section font implementation
    Reduce the font size of subtitle text. Use MUI styled utility to wrap the text for easier adjustment

--- components/common/FaqHowtoplay.tsx
+++ components/common/FaqHowtoplay.tsx
@@ -51,25 +51,25 @@ function FaqHowtoplay({ }: Props) {
 export default FaqHowtoplay;
 const FAQContent = () => (
     <Stack gap={3} maxHeight={"calc(100vh - 15rem)"}>
-        <Typography variant='h5' fontWeight={700} textAlign={'center'}>
+        <Title>
             FAQ
-        </Typography>
+        </Title>
         <Stack overflow={"auto"} gap={3}>
             <Stack gap={1}>
-                <Typography variant='body1' color='secondary.main' fontWeight={600}>
-                    What is deODD?
-                </Typography>
-                <Typography variant='body2' color="white" fontWeight={400} lineHeight={'20px'}>
+                <Subtitle>
+                    What is DeODD?
+                </Subtitle>
+                <ParagraphRegular>
                     DeODD is a smart contract built on the BNB Chain Ecosystem that allows users to play a decentralized coin flip
                     and lottery mechanism by your BNB, with the flipping odds of 50/50 and charge a 3.25% fee, and a portion
                     allocated to the DeODD NFT Holders Pool.
-                </Typography>
+                </ParagraphRegular>
             </Stack>
             <Stack gap={1}>
-                <Typography variant='body1' color='secondary.main' fontWeight={600}>
+                <Subtitle>
                     How much do I have to pay for a flip?
-                </Typography>
-                <Typography variant='body2' color="white" fontWeight={400} lineHeight={'20px'}>
+                </Subtitle>
+                <ParagraphRegular>
                     In addition to the bet amount, each flip you will have to pay the following fees: <br />
                     1. VRF fee (Verifiable Random Function): 0.003 BNB <br />
                     2. DeODD Services fee: 3.25% of bet amounts <br />
@@ -77,30 +77,30 @@ const FAQContent = () => (
                     <br />
                     For example: When you choose 0.01 BNB to flip, you actually spend: <br />
                     0.01 + 0.003 + 0.01*3.25% + 0.0007 = 0.014 BNB <Box component="span" fontStyle={"italic"}>(estimate)</Box>
-                </Typography>
+                </ParagraphRegular>
             </Stack>
             <Stack gap={1}>
-                <Typography variant='body1' color='secondary.main' fontWeight={600}>
+                <Subtitle>
                     How do I know I can Trust DeODD?
-                </Typography>
-                <Typography variant='body2' color="white" fontWeight={400} lineHeight={'20px'}>
+                </Subtitle>
+                <ParagraphRegular>
                     As a decentralized coin flip and lottery product, DeODD uses technology and platforms to guarantee a fair system,
                     especially with the use of Oracle VRF from the Binance Smart Chain ecosystem. This ensures transparency,
                     making these games an innovative and exciting way for users to enjoy blockchain technology.
-                </Typography>
+                </ParagraphRegular>
             </Stack>
             <Stack gap={1}>
-                <Typography variant='body1' color='secondary.main' fontWeight={600}>
+                <Subtitle>
                     Where can I learn the essentials?
-                </Typography>
-                <Typography variant='body2' color="white" fontWeight={400} lineHeight={'20px'}>
+                </Subtitle>
+                <ParagraphRegular>
                     Follow our social media channels to stay updated and/or Read our {" "}
                     <a href="https://docsend.com/view/nbwq8xa96nckf4zj" target='_blank' rel='noreferrer'>
                         <Typography display={"inline"} variant='body2' sx={{ textDecoration: "underline" }} >
                             WhitePaper
                         </Typography>
                     </a>.
-                </Typography>
+                </ParagraphRegular>
             </Stack>
         </Stack>
 
@@ -109,10 +109,10 @@ const FAQContent = () => (
 )
 const HowToPlay = () => (
     <Stack gap={3} maxHeight={"calc(100vh - 15rem)"}>
-        <Typography variant='h5' fontWeight={700} textAlign={'center'}>
+        <Title>
             How to play
-        </Typography>
-        <Typography variant='body2' color="white" fontWeight={400} lineHeight={'20px'}>
+        </Title>
+        <ParagraphRegular>
             Users need to follow these steps: <br />
             Step 1: Visit the official DeODD website <br />
             Step 2: Connect your Metamask Wallet (*). <br />
@@ -121,7 +121,7 @@ const HowToPlay = () => (
             Step 5: Select your bet amount with your BNB. <br />
             Step 6: Click &ldquo;Double or Nothing&rdquo;. <br />
             Step 7: Wait a few seconds and get the result. <br />
-        </Typography>
+        </ParagraphRegular>
         <Typography variant='body2' color="white" fontWeight={400} fontSize={"0.75rem"} lineHeight={'1rem'}>
             (*) Metamask Wallet: provides users with a secure and convenient way to manage their digital assets and interact with Dapps without the need for a separate wallet or complicated technical knowledge. Visit https://metamask.io/ and add the wallet to your Chrome, follow the instructions to create a wallet.
         </Typography>
@@ -130,25 +130,25 @@ const HowToPlay = () => (
 )
 const PrivacyPolicy = () => (
     <Stack gap={3} maxHeight={"min(100vh - 15rem, 30.75rem)"}>
-        <Typography variant='h5' fontSize={"1.5rem"} fontWeight={700} lineHeight={"2rem"} textAlign={'center'}>
+        <Title>
             Privacy Policy
-        </Typography>
+        </Title>
         <Stack gap={3} sx={{ maxHeight: "30rem", overflow: "auto" }}>
-            <Typography variant='body2' fontSize='0.875rem' lineHeight='1.25rem' color="#fff" fontWeight={400}>
+            <ParagraphRegular>
                 As a product based on a decentralized coin flip and lottery mechanism, the DeODD team (or &#34;We&#34;, &#34;Us&#34;) takes the privacy and security of our users&apos; information very seriously. This Privacy Policy section outlines the types of information we collect, how we use it, and the measures we take to protect it.
-            </Typography>
-            <Typography variant='body2' fontSize='0.875rem' lineHeight='1.25rem' color="#fff" fontWeight={400}>
+            </ParagraphRegular>
+            <ParagraphRegular>
                 We may be compelled to acquire your personal information in order to provide our services to you. Therefore, this Privacy Policy is meant to explain how we will collect and secure your personal information.
-            </Typography>
-            <Typography variant='body2' fontSize='0.875rem' lineHeight='1.25rem' color="#fff" fontWeight={400}>
+            </ParagraphRegular>
+            <ParagraphRegular>
                 This Privacy Policy does not apply to third-party products or services, or the activities of organizations we do not own or control, including other companies you may engage with on or through our services, unless otherwise stated.
-            </Typography>
-            <Typography variant='body2' fontSize='0.875rem' lineHeight='1.25rem' color="#fff" fontWeight={400}>
+            </ParagraphRegular>
+            <ParagraphRegular>
                 This Privacy Policy was created in the English language. When a translated version clashes with the English version, the English version takes precedence. The last time this Privacy Policy was updated was in May 2023.
-            </Typography>
-            <Typography variant='body2' fontSize='0.875rem' lineHeight='1.25rem' color="#fff" fontWeight={400}>
+            </ParagraphRegular>
+            <ParagraphRegular>
                 At any moment, we retain the right to make modifications to this Privacy Policy. We recommend that you review this Privacy Policy on a frequent basis to ensure that you are aware of any changes and how your information may be used.
-            </Typography>
+            </ParagraphRegular>
         </Stack>
     </Stack >
 
@@ -165,3 +165,25 @@ const Item = styled(Typography)(({ theme }) => ({
     color: (theme.palette.secondary as any)["500"],
     fontWeight: 500
 }))
+
+const Title = styled(Typography)(({ theme }) => ({
+    fontSize: "1.5rem",
+    lineHeight: "2rem",
+    textAlign: "center",
+    color: theme.palette.text.primary,
+    fontWeight: 700,
+}))
+
+const Subtitle = styled(Typography)(({ theme }) => ({
+    fontSize: "1rem",
+    lineHeight: "1.375rem",
+    color: theme.palette.text.secondary,
+    fontWeight: 600,
+}))
+
+const ParagraphRegular = styled(Typography)(({ theme }) => ({
+    fontSize: "0.875rem",
+    lineHeight: "1.25rem",
+    color: theme.palette.text.primary,
+    fontWeight: 400,
+})) 
