b4dc673c45e2b222bfca039252cdc692c693c827
Nam bv <nambv@versehub.io>
2023-05-17 16:05:46 +0700

    update short description

--- libs/utils/AppConfig.ts
+++ libs/utils/AppConfig.ts
@@ -4,6 +4,6 @@ export const AppConfig = {
   site_name: 'DeODD',
   company: 'Versehub',
   title: 'DeODD',
-  description: 'DeODD is a form of entertainment on Blockchain, we want our players to feel happy and enjoy participating at DeODD, so we encourage responsible gaming.',
+  description: 'DeODD is a smart contract built on the BNB Chain Ecosystem that allows users to play a decentralized coin flip and lottery mechanism by your BNB.',
   locale: 'en',
 };
