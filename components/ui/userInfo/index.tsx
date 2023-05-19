f692a6a84f5f0399faadd5774ac331974a32f263
Hà Ngọc Minh <hangoc.minh1306@gmail.com>
2023-05-19 17:59:21 +0700

    style: update user info expand animation

--- components/ui/userInfo/index.tsx
+++ components/ui/userInfo/index.tsx
@@ -15,7 +15,6 @@ import {
   LogoutIcon,
 } from "utils/Icons";
 import { Colors } from "constants/index";
-import Image from "next/image";
 import { useEffect, useState, useRef } from "react";
 import { Utils } from "@/utils/index";
 import { Convert } from "utils/convert";
@@ -139,9 +138,10 @@ export function UserInfo() {
         {/* Menu Container */}
         <Box
           sx={{
-            width: matchesScreen ? "auto" : expanded ? 1 : "auto",
+            minWidth: matchesScreen ? 0 : expanded ? 1 : 0,
             position: "absolute",
             right: 0,
+            transition: "300ms min-width",
           }}>
           <ProfileUsername open={isProfileOpened} onClose={() => { setIsProfileOpened(false) }} />
           {/* Summary Container */}
@@ -155,7 +155,7 @@ export function UserInfo() {
               backgroundColor: "primary.100",
               padding: { xs: expanded ? "0.875rem 0.75rem" : "0.5rem 0.75rem", md: "0.875rem 0.75rem" },
               gap: { xs: expanded ? 2 : 0, md: 2 },
-              transition: `box-shadow 300ms, 300ms gap, ${!expanded ? "300ms" : "0ms"} border-radius ${!expanded ? "150ms" : ""}`,
+              transition: `box-shadow 300ms, 300ms gap, 300ms padding, ${!expanded ? "300ms" : "0ms"} border-radius ${!expanded ? "150ms" : ""}`,
               borderRadius: expanded ? "8px 8px 0 0" : "8px",
               boxShadow: expanded ? "0 -2px 0 #3F4251, 2px 0px 0 #3F4251, -2px 0px 0 #3F4251" : "",
               cursor: "pointer",
@@ -163,11 +163,11 @@ export function UserInfo() {
           >
             <Stack direction="row" alignItems="center" sx={{ gap: { xs: expanded ? 1 : 0.5, md: 1 } }}>
               <Typography variant="h3" fontSize={"0.875rem"} fontWeight={500} lineHeight={"1.25rem"}>{Format.formatMoneyFromBigNumberEther(bnbBalance)}</Typography>
-              <BnbIcon fill={Colors.primaryDark} height={matchesScreen ? 20 : expanded ? 20 : 16} width={matchesScreen ? 20 : expanded ? 20 : 16} />
+              <BnbIcon fill={Colors.primaryDark} width={matchesScreen ? 20 : expanded ? 20 : 16} />
             </Stack>
             <Stack direction={"row"} alignItems="center" sx={{ gap: { xs: expanded ? 1 : 0.5, md: 1 } }}>
-              <Collapse in={!matchesScreen ? expanded ? true : false : true} orientation="horizontal" timeout={100}>
-                <Stack direction={"row"} gap={1} alignItems="center">
+              <Collapse in={!matchesScreen ? expanded ? true : false : true} orientation="horizontal" timeout={300}>
+                <Stack direction={"row"} gap={1} alignItems="center" >
                   <Typography fontSize={"0.875rem"} variant="h3" fontWeight={500} lineHeight={"1.25rem"}>
                     {
                       userInfo.username ? userInfo.username : Convert.convertWalletAddress(walletAddress, 5, 4)
@@ -185,48 +185,33 @@ export function UserInfo() {
                 color: matchesScreen ? "#F5F5FA" : expanded ? "#F5F5FA" : Colors.secondary
               }}>
                 <ArrowDownIcon
-                  height={matchesScreen ? 20 : expanded ? 20 : 16} width={matchesScreen ? 20 : expanded ? 20 : 16} />
+                  width={matchesScreen ? 20 : expanded ? 20 : 16} />
               </Stack>
             </Stack>
           </Stack>
 
           {/* Details Container */}
-          <Collapse in={expanded} sx={{
-            position: "absolute",
-            top: "100%",
-            right: 0,
-            width: 1,
-            backgroundColor: "primary.100",
-            borderRadius: "0 0 8px 8px",
-            transition: "300ms",
-            boxShadow: expanded ? "2px 0px 0 #3F4251, -2px 0px 0 #3F4251, 0px 2px 0 #3F4251" : "",
-          }}>
-            <Collapse
-              in={!matchesScreen ? expanded ? true : false : true}
-              orientation="horizontal"
-              timeout={100}
-              sx={{
-                ".MuiCollapse-wrapper": {
-                  flexFlow: "column nowrap",
-                  width: 1,
-                },
-                display: "flex",
-                justifyContent: "center",
-              }}
-            >
-              <Stack
-                sx={{
-                  gap: 1.5,
-                  opacity: expanded ? 1 : 0,
-                  transition: "opacity 600ms",
-                  padding: "0 0.75rem",
-                }}
-              >
-                <Stack direction={"row"} spacing={1}>
-                  <UserInfoButton onClick={() => { setIsProfileOpened(true) }} text="Profile" startIcon={<ProfileCircleIcon />} />
-                  <UserInfoButton href="/assets" text="Assets" startIcon={<ArchiveIcon />} />
-                </Stack>
-                {/* <Divider></Divider>
+          <Stack
+            sx={{
+              position: "absolute",
+              top: "100%",
+              width: 1,
+              maxHeight: expanded ? "100vh" : 0,
+              backgroundColor: "primary.100",
+              borderRadius: "0 0 8px 8px",
+              boxShadow: expanded ? "2px 0px 0 #3F4251, -2px 0px 0 #3F4251, 0px 2px 0 #3F4251" : "",
+              gap: 1.5,
+              transition: "max-height 300ms, box-shadow 300ms, opacity 300ms",
+              opacity: expanded ? 1 : 0,
+              overflow: "hidden",
+              padding: "0 0.75rem",
+            }}
+          >
+            <Stack direction={"row"} spacing={1}>
+              <UserInfoButton onClick={() => { setIsProfileOpened(true) }} text="Profile" startIcon={<ProfileCircleIcon />} />
+              <UserInfoButton href="/assets" text="Assets" startIcon={<ArchiveIcon />} />
+            </Stack>
+            {/* <Divider></Divider>
                   <Typography
                     variant="h3"
                     fontSize={"0.875rem"}
@@ -237,31 +222,29 @@ export function UserInfo() {
                   <Stack gap={1} divider={<Divider />}>
                     <Typography textAlign={'center'} variant="h3">Empty</Typography>
                   </Stack> */}
-                <Stack>
-                  <Divider></Divider>
-                  <Button
-                    variant="text"
-                    onClick={() => handleDisconnect()}
-                    startIcon={<LogoutIcon />}
-                    sx={{
-                      color: "secondary.400",
-                      fontSize: "0.75rem",
-                      border: "none",
-                      padding: "0.75rem 0",
-                      textTransform: "capitalize",
-                      "&:hover": {
-                        border: "none",
-                        color: "secondary.400",
-                        backgroundColor: "transparent"
-                      },
-                    }}
-                  >
-                    disconnect wallet
-                  </Button>
-                </Stack>
-              </Stack>
-            </Collapse>
-          </Collapse>
+            <Stack>
+              <Divider></Divider>
+              <Button
+                variant="text"
+                onClick={() => handleDisconnect()}
+                startIcon={<LogoutIcon />}
+                sx={{
+                  color: "secondary.400",
+                  fontSize: "0.75rem",
+                  border: "none",
+                  padding: "0.75rem 0",
+                  textTransform: "capitalize",
+                  "&:hover": {
+                    border: "none",
+                    color: "secondary.400",
+                    backgroundColor: "transparent"
+                  },
+                }}
+              >
+                disconnect wallet
+              </Button>
+            </Stack>
+          </Stack>
         </Box>
       </ClickAwayListener >
     );
