548b5abd48f0eb63bbe3152c72f9706bbf8f044c
Hà Ngọc Minh <hangoc.minh1306@gmail.com>
2023-05-24 17:48:13 +0700

    refactor: set container query to Main component

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -83,7 +83,7 @@ const Layout = ({ children }: IProps) => {
             <LeftSidebar mobileOpen={mobileOpenLeft} handleDrawerToggle={handleDrawerToggleLeft} open={leftOpen} />
             <Main rightOpen={rightOpen} leftOpen={leftOpen}>
                 <DrawerHeader />
-                <main className="mainLayout">
+                <main>
                     {children}
                 </main>
 
