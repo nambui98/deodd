ff998cc7c69a77c6f9002d268be5f28e8f28835d
Nam bv <nambv@versehub.io>
2023-05-25 16:36:49 +0700

    refactor

---
+++ hooks/useDeoddNFTContract.ts
@@ -0,0 +1,153 @@
+import { BigNumber } from "ethers";
+import { useEffect, useState } from "react";
+import { useWalletContext } from "../contexts/WalletContext";
+
+import { useSiteContext } from "contexts/SiteContext";
+import { getPriceToken } from "libs/apis/coinmarketcap";
+import { EnumNFT } from "libs/types";
+export type TypeNFT = {
+    id: number | string,
+    type: number | string,
+    amount: number,
+}
+
+export type TypeDataNFT = {
+    total: number | undefined;
+    data: {
+        type: EnumNFT,
+        list: TypeNFT[]
+    }[];
+
+} | undefined;
+
+export const useDeoddNFTContract = () => {
+    const { walletAddress } = useWalletContext();
+    const [walletTokens, setWalletTokens] = useState<TypeDataNFT>();
+    const [nftSelected, setNftSelected] = useState<TypeNFT | undefined>();
+    const [reload, setReload] = useState<boolean>(false);
+    const { setIsLoading, setIsError, setTitleError, setTitleSuccess, setIsSuccess } = useSiteContext();
+    const [priceToken, setPriceToken] = useState<number | undefined>();
+
+    const getSpendingTokens = async () => {
+        // const res = await contractDeoddNft?.getSpendingTokens(walletAddress)
+        // const data = getInfoTokens(res);
+        // return data;
+    };
+    const getTokenTypeId = async (id: BigNumber) => {
+        // const res = await contractDeoddNft?.getTokenTypeId(id)
+        // return res;
+    };
+    const getWalletTokens = async () => {
+        // const res = await contractDeoddNft?.getWalletTokens(walletAddress)
+        // const data = getInfoTokens(res);
+        // return data;
+    };
+    const getInfoTokens = async (tokens: BigNumber[]) => {
+        // let res = await Promise.all(
+        //     (tokens ?? []).map(async (token) => {
+        //         const type: BigNumber = await getTokenTypeId(token);
+        //         token = BigNumber.from(token);
+        //         const nft: TypeNFT = {
+        //             id: token.toNumber(),
+        //             type: type.toNumber(),
+        //             amount: 1
+        //         }
+        //         return nft;
+        //     })
+        // )
+        //     .then((res) => {
+        //         const data = aggregateQuantity(res);
+        //         return data;
+        //     })
+        //     .catch((err) => {
+        //         return;
+        //     });
+        // return res;
+    }
+
+    const aggregateQuantity = (arr: TypeNFT[]) => {
+        // let total: number = arr.length;
+        // let dataBronze: {
+        //     type: EnumNFT,
+        //     list: TypeNFT[]
+        // } | undefined = {
+        //     type: EnumNFT.BRONZE,
+        //     list: []
+        // };
+        // let dataGold: {
+        //     type: EnumNFT,
+        //     list: TypeNFT[]
+        // } | undefined = {
+        //     type: EnumNFT.GOLD,
+        //     list: []
+        // };
+        // let dataDiamond: {
+        //     type: EnumNFT,
+        //     list: TypeNFT[]
+        // } | undefined = {
+        //     type: EnumNFT.DIAMOND,
+        //     list: []
+        // };
+        // for (let index = 0; index < arr.length; index++) {
+        //     const item = arr[index];
+        //     if (item.type === EnumNFT.BRONZE) {
+        //         dataBronze?.list.push(item);
+        //     } else if (item.type === EnumNFT.GOLD) {
+        //         dataGold?.list.push(item);
+        //     } else {
+        //         dataDiamond?.list.push(item);
+        //     }
+
+        // }
+        // return {
+        //     total: total,
+        //     data: [
+        //         { ...dataBronze },
+        //         { ...dataGold },
+        //         { ...dataDiamond }
+        //     ]
+        // }
+    }
+
+    useEffect(() => {
+        if (walletAddress) {
+            // getWalletTokens().then(res => {
+            //     if (res && res.data.length > 0) {
+            //         setWalletTokens(res);
+            //     }
+            // });
+            getPriceToken().then(res => {
+                if (res.status === 200) {
+                    setPriceToken(res.data.data[0].quote['BUSD'].price)
+                }
+            })
+
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [reload, walletAddress])
+
+    const handleClickNFT = (nft: TypeNFT) => {
+        setNftSelected(nft)
+    }
+    const claimToWallet = async () => {
+        // const res = await contractDeoddNft?.claimToWallet(BigNumber.from(nftSelected?.id));
+        // return res.wait();
+    }
+    const handleClaimNFT = async () => {
+        // try {
+        //     setIsLoading(true);
+        //     let res = await claimToWallet();
+        //     setIsLoading(false);
+        //     if (res.status) {
+        //         setTitleSuccess('Claimed successfully')
+        //         setIsSuccess(true);
+        //         setReload(!reload);
+        //     }
+        // } catch (error: any) {
+        //     setIsLoading(false);
+        //     setTitleError(error.reason || 'Something went wrong. Please try again!')
+        //     setIsError(true);
+        // }
+    }
+    return { walletTokens, handleClickNFT, nftSelected, handleClaimNFT, priceToken }
+}
