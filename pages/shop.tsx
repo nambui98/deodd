f83a4e211cc0e4eb6c6e40912e2d5d4bc329f538
Hà Ngọc Minh <hangoc.minh1306@gmail.com>
2023-06-06 10:16:30 +0700

    add connect wallet UI for shop page

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,12 +1,22 @@
-import { Typography } from '@mui/material'
+import { Typography, Container } from '@mui/material'
 import React from 'react'
+import ShopConnectWallet from '@/templates/shop/ShopConnectWallet';
+import { useWalletContext } from 'contexts/WalletContext';
 
 type Props = {}
 
 export default function Shop({ }: Props) {
+    const { walletIsConnected } = useWalletContext();
+
+    // return (
+    //     <Typography variant='h2' mx="auto" mt={4} textAlign={'center'}>
+    //         Coming soon
+    //     </Typography>
+    // )
+
     return (
-        <Typography variant='h2' mx="auto" mt={4} textAlign={'center'}>
-            Coming soon
-        </Typography>
-    )
+        <Container>
+            {walletIsConnected ? <></> : <ShopConnectWallet />}
+        </Container>
+    );
 }
--- utils/Icons.tsx
+++ utils/Icons.tsx
@@ -52,6 +52,7 @@ import { SvgIcon, SvgIconProps } from "@mui/material";
 import CloseSquareIcon2 from "public/assets/icons/close-square2.svg";
 import InfoCircleIcon from "public/assets/icons/info-circle.svg";
 import EmojiIcon from "public/assets/icons/emoji-happy.svg";
+import Marketplace1Icon from "public/assets/icons/marketplace1.svg";
 function Bnb2Icon(props: SvgIconProps) {
   return (
     <SvgIcon {...props}>
@@ -116,5 +117,6 @@ export {
   ProfileCircleIcon,
   ArchiveIcon,
   CloseSquareIcon2,
-  InfoCircleIcon
+  InfoCircleIcon,
+  Marketplace1Icon,
 };
