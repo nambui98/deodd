f03efd878c8941ccc9adce2f4ba28adbf2f98d46
Nam bv <nambv@versehub.io>
2023-06-23 10:17:58 +0700

    add GA for environment dev

--- utils/gtag.ts
+++ utils/gtag.ts
@@ -1,8 +1,6 @@
 // export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || 'G-0EWD6WDSBY'
 export const GA_TRACKING_ID = 'G-7LRDKCTDGQ'
 export const GA_TRACKING_ID_DEV = 'G-DS42L7MYPV'
-
-// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageView = (url: URL) => {
   window.gtag("config", process.env.NEXT_PUBLIC_ENVIRONMENT === "PRODUCTION" ? GA_TRACKING_ID : GA_TRACKING_ID_DEV, {
     page_path: url
