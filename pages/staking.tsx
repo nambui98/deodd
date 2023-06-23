8cfa8e538464d968e4b017631dec3e222cc9a067
Hà Ngọc Minh <hangoc.minh1306@gmail.com>
2023-06-23 09:58:02 +0700

    create link and icon for staking page
    Insert staking page icon and link into left sidebar.

---
+++ pages/staking.tsx
@@ -0,0 +1,13 @@
+import { Container } from "@mui/material";
+
+function StakingPage() {
+
+  return (
+    <Container sx={{ mb: 10, p: { xs: 0, md: 3 } }}>
+    </Container>
+  );
+}
+
+export default StakingPage;
+
+
