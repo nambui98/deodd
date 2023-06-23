aa2930d771d169a3785a8fa5248f3cab4c897de9
Hà Ngọc Minh <hangoc.minh1306@gmail.com>
2023-06-23 10:32:07 +0700

    update staking page UI
    Add UI for staking page when users have not connected their wallet

--- pages/staking.tsx
+++ pages/staking.tsx
@@ -1,9 +1,15 @@
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
+import { useWalletContext } from "contexts/WalletContext";
+import StakingNoWallet from "@/templates/staking/StakingNoWallet";
 
 function StakingPage() {
+  const { walletIsConnected } = useWalletContext();
 
   return (
-    <Container sx={{ mb: 10, p: { xs: 0, md: 3 } }}>
+    <Container sx={{ mt: 10, mb: { md: 10, xs: 8.125 }, mx: "auto", width: 736 }}>
+      {/* Change Me */}
+      <Typography variant="h2" sx={{ fontWeight: 700, lineHeight: "2rem", mb: 2 }}>NFT Staking</Typography>
+      {walletIsConnected ? null : <StakingNoWallet />}
     </Container>
   );
 }
