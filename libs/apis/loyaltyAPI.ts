bc25c4634e8846b510a66c07c9977b7f50b746ad
Hà Ngọc Minh <hangoc.minh1306@gmail.com>
2023-06-29 16:42:45 +0700

    remove unnecessary API

--- libs/apis/loyaltyAPI.ts
+++ libs/apis/loyaltyAPI.ts
@@ -42,41 +42,6 @@ export const getLoyaltyHistoryJackpot = async (
   });
 };
 
-export const getLoyaltyNFTCurrent = async (
-  wallet: string,
-  signal?: AbortSignal
-) => {
-  return vhIdRequest({
-    url: baseURL + `/nft/board/current?wallet=${wallet}`,
-    method: "get",
-    signal: signal,
-  });
-};
-
-export const getLoyaltyNFTBoardBySeason = async (
-  wallet: string,
-  season: number,
-  signal?: AbortSignal
-) => {
-  return vhIdRequest({
-    url: baseURL + `/nft/board/season?wallet=${wallet}&season=${season}`,
-    method: "get",
-    signal: signal,
-  });
-};
-
-export const getNFTItemProfitBySeason = async (
-  wallet: string,
-  season: number,
-  signal?: AbortSignal
-) => {
-  return vhIdRequest({
-    url: baseURL + `/nft/profit/season?wallet=${wallet}&season=${season}`,
-    method: "get",
-    signal: signal,
-  });
-};
-
 export const claimNFTReward = async (address: string) => {
   return await vhIdRequest({
     url: baseURL + `/users/nft/claim`,
@@ -90,13 +55,6 @@ export const claimNFTReward = async (address: string) => {
 export const getJackpotBoom = async () => {
   return await vhIdRequest({
     url: baseURL + `/jackpot/boom`,
-    method: 'get',
-  })
-}
-
-export const getCurrentNftPool = async () => {
-  return await vhIdRequest({
-    url: baseURL + `/nft/staking/current-pool`,
     method: 'get',
   })
 }
