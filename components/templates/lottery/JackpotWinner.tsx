bf54fea60aa5cf87bbe1d4c2823f2fdadfbb2948
Nam bv <nambv@versehub.io>
2023-08-03 16:40:49 +0700

    fix deploy

--- components/templates/lottery/JackpotWinner.tsx
+++ components/templates/lottery/JackpotWinner.tsx
@@ -23,10 +23,10 @@ const JackpotWinner = (props: Props) => {
             <Typography variant='h5' fontWeight={700} mt={5}>Jackpot Winner</Typography>
             <Box mt={3}>
                 <Stack display={{ xs: 'flex', md: 'none' }} divider={<Divider sx={{ my: 2 }} />}>
-                    <ResultTicketInfo />
-                    <ResultTicketInfo />
-                    <ResultTicketInfo />
-                    <ResultTicketInfo />
+                    <ResultTicketInfo data={undefined} />
+                    <ResultTicketInfo data={undefined} />
+                    <ResultTicketInfo data={undefined} />
+                    <ResultTicketInfo data={undefined} />
                 </Stack>
                 <Box display={{ xs: 'none', md: 'block' }}>
 
