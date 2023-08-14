80e16c26fc69f33f3f58764dc8983ce02b434a1a
Nam bv <nambv@versehub.io>
2023-08-14 16:01:58 +0700

    claim lottery

--- components/templates/lottery/components/ModalProvablyFair.tsx
+++ components/templates/lottery/components/ModalProvablyFair.tsx
@@ -96,16 +96,16 @@ const ModalProvablyFair = (props: Props) => {
         {
           randomValues?.map(({ key, value, valueIsExits }, index) => <Fragment key={key}>
             <Grid item xs={8} md={6}>
-              <Stack direction={'row'} gap={{ xs: 4, md: 13.5 }} alignItems={'center'}>
-                <Typography variant='body2' width={"50%"} fontWeight={500}>{Convert.convertWalletAddress(key.toString(), 4, 4)}</Typography>
+              <Stack direction={'row'} justifyContent={'space-between'} alignItems={'center'}>
+
+                <Typography variant='body2' fontWeight={500}>{Convert.convertWalletAddress(key.toString(), 4, 4)}</Typography>
                 {
                   index !== randomValues.length - 1 ?
 
                     <Stack position={'relative'}>
 
                       <Typography variant='caption' color="secondary.100" fontWeight={400} position={'absolute'} top={-16} left={'50%'} sx={{ transform: 'translateX(-50%)' }}>
-                        %25
-
+                        mode 25 + 1
                       </Typography>
                       <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="6" viewBox="0 0 147 6" fill="none">
                         <path d="M1 2.5C0.723858 2.5 0.5 2.72386 0.5 3C0.5 3.27614 0.723858 3.5 1 3.5V2.5ZM147 3L142 0.113249V5.88675L147 3ZM1 3.5H5.05556V2.5H1V3.5ZM13.1667 3.5H21.2778V2.5H13.1667V3.5ZM29.3889 3.5H37.5V2.5H29.3889V3.5ZM45.6111 3.5H53.7222V2.5H45.6111V3.5ZM61.8333 3.5H69.9444V2.5H61.8333V3.5ZM78.0556 3.5H86.1667V2.5H78.0556V3.5ZM94.2778 3.5H102.389V2.5H94.2778V3.5ZM110.5 3.5H118.611V2.5H110.5V3.5ZM126.722 3.5H134.833V2.5H126.722V3.5Z" fill="#96A5C0" />
@@ -115,7 +115,7 @@ const ModalProvablyFair = (props: Props) => {
                     <Stack position={'relative'}>
 
                       <Typography variant='caption' color="secondary.main" fontWeight={400} position={'absolute'} top={-16} left={'50%'} sx={{ transform: 'translateX(-50%)' }}>
-                        %10
+                        mode 10 + 1
                       </Typography>
                       <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="6" viewBox="0 0 147 6" fill="none">
                         <path d="M1 2.5C0.723858 2.5 0.5 2.72386 0.5 3C0.5 3.27614 0.723858 3.5 1 3.5V2.5ZM147 3L142 0.113249V5.88675L147 3ZM1 3.5H5.05556V2.5H1V3.5ZM13.1667 3.5H21.2778V2.5H13.1667V3.5ZM29.3889 3.5H37.5V2.5H29.3889V3.5ZM45.6111 3.5H53.7222V2.5H45.6111V3.5ZM61.8333 3.5H69.9444V2.5H61.8333V3.5ZM78.0556 3.5H86.1667V2.5H78.0556V3.5ZM94.2778 3.5H102.389V2.5H94.2778V3.5ZM110.5 3.5H118.611V2.5H110.5V3.5ZM126.722 3.5H134.833V2.5H126.722V3.5Z" fill={Colors.secondaryDark} />
